13cb62e9427ae74295daca9ea25d760659ab430f
AnsElwkel <anshesh039@gmail.com>
2025-03-12 13:13:00 +0200

    Update PathFinder.cs

--- src/Model/PathFinder.cs
+++ src/Model/PathFinder.cs
@@ -10,16 +10,15 @@ namespace Map_Creation_Tool.src.Model
         private static readonly int INF = 1_000_000_00;
         private XY_Point fromPoint, toPoint;
         private static List<XY_Point> path;
+        private static readonly int[] dx = { 0, 1, 0, -1 , 1, 1, -1, -1};
+        private static readonly int[] dy = { 1, 0, -1, 0 , 1 ,-1 , 1, -1};
 
         public PathFinder(XY_Point fromPoint , XY_Point toPoint)
 		{
             this.fromPoint = fromPoint;
             this.toPoint = toPoint;
-            
         }
         
-        private int[] dx = { 0, 1, 0, -1 , 1, 1, -1, -1};
-        private int[] dy = { 1, 0, -1, 0 , 1 ,-1 , 1, -1};
 
         class Node : IComparable<Node>
         {
@@ -46,7 +45,7 @@ namespace Map_Creation_Tool.src.Model
         private int euclidean_distance(XY_Point from , XY_Point to)
         {
             return (int)Math.Sqrt(Math.Pow(from.X - to.X, 2) + Math.Pow(from.Y - to.Y, 2));
-        }
+        } 
 
         //Calaculate the weight of the cell based on RGB values
         public int calculateWeight(int row , int col)
@@ -70,6 +69,7 @@ namespace Map_Creation_Tool.src.Model
             int[,] len = new int[rows, cols];
             Dictionary<XY_Point, XY_Point> parent = new Dictionary<XY_Point, XY_Point>();
             PriorityQueue<Node, int> pq = new PriorityQueue<Node, int>();
+            bool[,] visited = new bool[rows, cols];
 
             //initialize the len array with INF value
             for (int i = 0; i < rows; i++)
@@ -91,7 +91,12 @@ namespace Map_Creation_Tool.src.Model
                 //Reach the target break then build the path
                 if (curNode.point.Equals(toPoint))
                     break;
-                
+
+                if (visited[curNode.point.X, curNode.point.Y])
+                    continue;
+
+                visited[curNode.point.X, curNode.point.Y] = true;
+
                 for (int i = 0; i < 8; i++)
                 {
                     int nX = curNode.point.X + dx[i];
@@ -105,8 +110,7 @@ namespace Map_Creation_Tool.src.Model
 
                             XY_Point curPoint = new XY_Point(nX, nY);
                             pq.Enqueue(new Node(curPoint, newLen, euclidean_distance(curPoint, toPoint)), newLen);
-
-                            parent.Add(curPoint, curNode.point);
+                            parent[curPoint] = curNode.point;
                         }
                     }
                 }
@@ -121,8 +125,12 @@ namespace Map_Creation_Tool.src.Model
             while (curPoint.X != -1 && curPoint.Y != -1)
             {
                 path.Add(curPoint);
-                curPoint.X = parent[curPoint].X;
-                curPoint.Y = parent[curPoint].Y;
+                if (!parent.ContainsKey(curPoint))
+                {
+                    Console.WriteLine($"Doesn't exist {curPoint.X}::{curPoint.Y}");
+                    break;
+                }
+                curPoint = parent[curPoint];
             }
 
             path.Reverse();
